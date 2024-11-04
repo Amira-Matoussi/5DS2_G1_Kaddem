b33b7d7f4d5fe23ae79f8cddc3c2edc00d123ebc
Hamza-jl <hamza.jelassi@esprit.tn>
2024-11-04 23:38:58 +0100

    first commit

--- src/test/java/tn/esprit/spring/TestDetailEquipeImpl.java
+++ src/test/java/tn/esprit/spring/TestDetailEquipeImpl.java
@@ -58,7 +58,7 @@ import tn.esprit.spring.kaddem.entities.Equipe;
         detailEquipe.setEquipe(equipe);
         assertEquals(equipe, detailEquipe.getEquipe());
     }
-
+//test
     @Test
      void testDetailEquipeConstructor() {
         DetailEquipe newDetailEquipe = new DetailEquipe(2, 201, "Team Tactics");
