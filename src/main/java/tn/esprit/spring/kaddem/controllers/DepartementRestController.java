5953bf2a446db8c2cf7bfda9902b393de0ac2770
Zaineb Boujelbene <boujelbenezaineb@gmail.com>
2024-11-04 23:01:00 +0100

    correcting more bad practises

--- src/main/java/tn/esprit/spring/kaddem/controllers/DepartementRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/DepartementRestController.java
@@ -3,10 +3,10 @@ package tn.esprit.spring.kaddem.controllers;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.kaddem.entities.Departement;
+import tn.esprit.spring.kaddem.entities.DepartementDTO;
 import tn.esprit.spring.kaddem.services.IDepartementService;
 
 import java.util.List;
-
 @RestController
 @AllArgsConstructor
 @RequestMapping("/departement")
@@ -15,8 +15,7 @@ public class DepartementRestController {
 	// http://localhost:8089/Kaddem/departement/retrieve-all-departements
 	@GetMapping("/retrieve-all-departements")
 	public List<Departement> getDepartements() {
-		List<Departement> listDepartements = departementService.retrieveAllDepartements();
-		return listDepartements;
+		return departementService.retrieveAllDepartements();
 	}
 	// http://localhost:8089/Kaddem/departement/retrieve-departement/8
 	@GetMapping("/retrieve-departement/{departement-id}")
@@ -26,9 +25,10 @@ public class DepartementRestController {
 
 	// http://localhost:8089/Kaddem/departement/add-departement
 	@PostMapping("/add-departement")
-	public Departement addDepartement(@RequestBody Departement d) {
-		Departement departement = departementService.addDepartement(d);
-		return departement;
+	public Departement addDepartement(@RequestBody DepartementDTO departementDTO) {
+		Departement departement = new Departement();
+		departement.setNomDepart(departementDTO.getNomDepart());
+		return departementService.addDepartement(departement);
 	}
 
 	// http://localhost:8089/Kaddem/departement/remove-departement/1
@@ -39,10 +39,14 @@ public class DepartementRestController {
 
 	// http://localhost:8089/Kaddem/departement/update-departement
 	@PutMapping("/update-departement")
-	public Departement updateDepartement(@RequestBody Departement e) {
-		Departement departement= departementService.updateDepartement(e);
-		return departement;
+	public Departement updateDepartement(@RequestBody DepartementDTO departementDTO) {
+		Departement departement = new Departement();
+		departement.setIdDepart(departementDTO.getIdDepart());
+		departement.setNomDepart(departementDTO.getNomDepart());
+
+		// Add any other fields as necessary
+
+		return departementService.updateDepartement(departement);
 	}
 }
-
 
--- src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
@@ -4,21 +4,26 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.entities.EtudiantDTO;
+import tn.esprit.spring.kaddem.entities.Option;
 import tn.esprit.spring.kaddem.services.IEtudiantService;
 
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping("/etudiant")
 public class EtudiantRestController {
+	private final IEtudiantService etudiantService;
+
+	// Constructor injection
 	@Autowired
-	IEtudiantService etudiantService;
+	public EtudiantRestController(IEtudiantService etudiantService) {
+		this.etudiantService = etudiantService;
+	}
 	// http://localhost:8089/Kaddem/etudiant/retrieve-all-etudiants
 	@GetMapping("/retrieve-all-etudiants")
 	public List<Etudiant> getEtudiants() {
-		List<Etudiant> listEtudiants = etudiantService.retrieveAllEtudiants();
-		return listEtudiants;
+		return etudiantService.retrieveAllEtudiants();
 	}
 	// http://localhost:8089/Kaddem/etudiant/retrieve-etudiant/8
 	@GetMapping("/retrieve-etudiant/{etudiant-id}")
@@ -28,9 +33,14 @@ public class EtudiantRestController {
 
 	// http://localhost:8089/Kaddem/etudiant/add-etudiant
 	@PostMapping("/add-etudiant")
-	public Etudiant addEtudiant(@RequestBody Etudiant e) {
-		Etudiant etudiant = etudiantService.addEtudiant(e);
-		return etudiant;
+	public Etudiant addEtudiant(@RequestBody EtudiantDTO eDTO) {
+		// Create a new Etudiant entity
+		Etudiant etudiant = new Etudiant();
+		etudiant.setNomE(eDTO.getNomE());
+		etudiant.setPrenomE(eDTO.getPrenomE());
+		etudiant.setOp(Option.valueOf(eDTO.getOp())); // Assuming Option can be set this way
+
+		return etudiantService.addEtudiant(etudiant);
 	}
 
 	// http://localhost:8089/Kaddem/etudiant/remove-etudiant/1
@@ -41,25 +51,36 @@ public class EtudiantRestController {
 
 	// http://localhost:8089/Kaddem/etudiant/update-etudiant
 	@PutMapping("/update-etudiant")
-	public Etudiant updateEtudiant(@RequestBody Etudiant e) {
-		Etudiant etudiant= etudiantService.updateEtudiant(e);
+	public Etudiant updateEtudiant(@RequestBody EtudiantDTO eDTO) {
+		// Create a new Etudiant entity
+		Etudiant etudiant = new Etudiant();
+		etudiant.setIdEtudiant(eDTO.getIdEtudiant()); // Set the ID for updating
+		etudiant.setNomE(eDTO.getNomE());
+		etudiant.setPrenomE(eDTO.getPrenomE());
+		etudiant.setOp(Option.valueOf(eDTO.getOp())); // Assuming Option can be set this way
 
-		return etudiant;
+		return etudiantService.updateEtudiant(etudiant);
 	}
 
 	//@PutMapping("/affecter-etudiant-departement")
 	@PutMapping(value="/affecter-etudiant-departement/{etudiantId}/{departementId}")
 	public void affecterEtudiantToDepartement(@PathVariable("etudiantId") Integer etudiantId, @PathVariable("departementId")Integer departementId){
 		etudiantService.assignEtudiantToDepartement(etudiantId, departementId);
-    }
-//addAndAssignEtudiantToEquipeAndContract(Etudiant e, Integer idContrat, Integer idEquipe)
-    /* Ajouter un étudiant tout en lui affectant un contrat et une équipe */
-    @PostMapping("/add-assign-Etudiant/{idContrat}/{idEquipe}")
-    @ResponseBody
-    public Etudiant addEtudiantWithEquipeAndContract(@RequestBody Etudiant e, @PathVariable("idContrat") Integer idContrat, @PathVariable("idEquipe") Integer idEquipe) {
-        Etudiant etudiant = etudiantService.addAndAssignEtudiantToEquipeAndContract(e,idContrat,idEquipe);
-        return etudiant;
-    }
+	}
+	//addAndAssignEtudiantToEquipeAndContract(Etudiant e, Integer idContrat, Integer idEquipe)
+	@PostMapping("/add-etudiant/{idContrat}/{idEquipe}")
+	public Etudiant addEtudiantWithEquipeAndContract(@RequestBody EtudiantDTO eDTO,
+													 @PathVariable("idContrat") Integer idContrat,
+													 @PathVariable("idEquipe") Integer idEquipe) {
+		// Create a new Etudiant entity from the DTO
+		Etudiant etudiant = new Etudiant();
+		etudiant.setNomE(eDTO.getNomE());
+		etudiant.setPrenomE(eDTO.getPrenomE());
+		etudiant.setOp(Option.valueOf(eDTO.getOp())); // Adjust according to your Option enum
+
+		// Call the service method to add and assign the student to the contract and team
+		return etudiantService.addAndAssignEtudiantToEquipeAndContract(etudiant, idContrat, idEquipe);
+	}
 
 	@GetMapping(value = "/getEtudiantsByDepartement/{idDepartement}")
 	public List<Etudiant> getEtudiantsParDepartement(@PathVariable("idDepartement") Integer idDepartement) {
